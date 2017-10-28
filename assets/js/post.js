2cfbba060da623bb97de6a6cdc5850755f7596ee
ShinCurry <shincurryyang@gmail.com>
2017-10-28 23:52:39 +0800

    use Han.css and migrating to Sass

--- assets/js/post.js
+++
@@ -1,9 +0,0 @@
-hljs.configure({
-    tabReplace: '    ',
-    classPrefix: '' 
-})
-hljs.initHighlightingOnLoad()
-var links=document.querySelectorAll('.post a');
-for (var i = links.length - 1; i >= 0; i--) {
-	links[i].setAttribute('target','_blank')
-}
